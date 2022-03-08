2c570d2dd69adc7eba259497750dabbd3caa5232
pannich <70697272+pannich@users.noreply.github.com>
2022-03-08 17:21:19 +0000

    test flipping

--- streamlit_hadjer.py
+++ streamlit_hadjer.py
@@ -8,12 +8,16 @@ from streamlit_webrtc import (
     VideoProcessorBase,
     WebRtcMode,
     webrtc_streamer,
+    VideoTransformerBase
 )
 
 import cv2
 from PIL import Image,ImageEnhance
 import numpy as np
 import os
+
+import threading
+from typing import Union
 
 
 #Import for handling image
@@ -143,6 +147,9 @@ def app_emotion_detection():
         async_processing=True,
     )
 
+ 
+ 
+    
 
 ############################ Sidebar + launching #################################################
 
@@ -179,7 +186,46 @@ def main():
     elif choice ==  "Live Emotion Detector":
         app_emotion_detection()
 
+        
+    class VideoTransformer(VideoTransformerBase):
+        frame_lock: threading.Lock  # `transform()` is running in another thread, then a lock object is used here for thread-safety.
+        in_image: Union[np.ndarray, None]
+        out_image: Union[np.ndarray, None]
 
+        def __init__(self) -> None:
+            self.frame_lock = threading.Lock()
+            self.in_image = None
+            self.out_image = None
+
+        def transform(self, frame: av.VideoFrame) -> np.ndarray:
+            in_image = frame.to_ndarray(format="bgr24")
+
+            out_image = in_image[:, ::-1, :]  # Simple flipping for example.
+
+            with self.frame_lock:
+                self.in_image = in_image
+                self.out_image = out_image
+
+            return out_image
+
+    ctx = webrtc_streamer(key="snapshot", video_transformer_factory=VideoTransformer)
+
+    if ctx.video_transformer:
+        if st.button("Snapshot"):
+            with ctx.video_transformer.frame_lock:
+                in_image = ctx.video_transformer.in_image
+                out_image = ctx.video_transformer.out_image
+
+            if in_image is not None and out_image is not None:
+                st.write("Input image:")
+                st.image(in_image, channels="BGR")
+                st.write("Output image:")
+                st.image(out_image, channels="BGR")
+            else:
+                st.warning("No frames available yet.")
+        
+        
+      
 if __name__ == '__main__':
     main()
 
